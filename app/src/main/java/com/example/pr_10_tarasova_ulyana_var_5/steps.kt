77c83d6080aa7d9f8a1be0b375ec3ceb93dc3efb
ULTR5RM <96973326+ULTR5RM@users.noreply.github.com>
2022-05-09 12:49:38 +0700

    страница steps

---
+++ app/src/main/java/com/example/pr_10_tarasova_ulyana_var_5/steps.kt
@@ -0,0 +1,11 @@
+package com.example.pr_10_tarasova_ulyana_var_5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class steps : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_steps)
+    }
+}
